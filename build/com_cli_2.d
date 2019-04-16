8649d8afdee620d464c9b2afdd4035050286a7bc
goroyabu <goroyabu@goroyabuMBA.local>
2019-04-16 20:07:32 +0900

    first

--- build/com_cli_2.d
+++
@@ -1,8 +0,0 @@
-/Users/goroyabu/github/com_cli/1.0/build/com_cli_2.o: \
-  /Users/goroyabu/github/com_cli/1.0/source/com_cli_2.cpp \
-  /Users/goroyabu/github/com_cli/1.0/source/com_cli.hpp \
-  /Users/goroyabu/github/com_cli/1.0/source/com_cli_private.hpp
-
-/Users/goroyabu/github/com_cli/1.0/source/com_cli.hpp:
-
-/Users/goroyabu/github/com_cli/1.0/source/com_cli_private.hpp:
